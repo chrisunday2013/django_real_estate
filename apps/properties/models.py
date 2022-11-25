dfe551b8a6a3327da7615f6d4db4e22f06b9dbe7
Christian Sunday <potentialsunny47@gmail.com>
2022-11-25 03:15:22 +0100

    enquiries models and serializers

--- apps/properties/models.py
+++ apps/properties/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator 
 from django.db import models 
 from django.utils.translation import gettext_lazy as _ 
-from django_countries import CountryField 
+from django_countries.fields import CountryField
 from apps.common.models import TimeStampedUUIDModel 
 
 User = get_user_model()
